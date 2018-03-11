2b172602adacda8f4c4d9ce5ecbf78180c080cae
Maurice Domínguez <maurice.ronet.dominguez@gmail.com>
2018-03-11 19:11:05 +0100

    Update: Restore user permisions in report files

--- generators/app/templates/bin/test.sh
+++ generators/app/templates/bin/test.sh
@@ -7,3 +7,5 @@ docker run \
 -e NODE=8 \
 -v `pwd`:/workspace \
 redpandaci/npm-command-runner:1.2.0
+
+docker run --rm -v `pwd`:/home ubuntu chown -R --reference=/home/Jenkinsfile /home # try to restore permissions
